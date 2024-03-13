a75876a1a28446a8a6baa74975311fe6a98d91bb
TFoley <trudood05@gmail.com>
2024-03-13 19:04:05 -0400

    Initial commit

---
+++ src/main/java/net/tfoley/join_the_illagers/JoinTheIllagersClient.java
@@ -0,0 +1,10 @@
+package net.tfoley.join_the_illagers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JoinTheIllagersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/tfoley/join_the_illagers/JoinTheIllagersDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tfoley.join_the_illagers;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JoinTheIllagersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
