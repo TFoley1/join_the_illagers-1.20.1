b2fbf305f9a88bbef7ed5fae77d162cd8f9aa7fc
TFoley <trudood05@gmail.com>
2024-03-13 21:34:36 -0400

    Testing and setup...

--- src/main/java/net/tfoley/join_the_illagers/JoinTheIllagers.java
+++ src/main/java/net/tfoley/join_the_illagers/JoinTheIllagers.java
@@ -2,14 +2,20 @@ package net.tfoley.join_the_illagers;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tfoley.join_the_illagers.block.ModBlocks;
+import net.tfoley.join_the_illagers.item.ModItemGroups;
+import net.tfoley.join_the_illagers.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 public class JoinTheIllagers implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String MOD_ID = "join_the_illagers";
+	public static File icon = new File("join_the_illagers/src/resources/assets/icon.png");
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
@@ -18,6 +24,9 @@ public class JoinTheIllagers implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		//LOGGER.info("Hello Fabric world!");
+
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
--- src/main/java/net/tfoley/join_the_illagers/JoinTheIllagersDataGenerator.java
+++ src/main/java/net/tfoley/join_the_illagers/JoinTheIllagersDataGenerator.java
@@ -2,10 +2,18 @@ package net.tfoley.join_the_illagers;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.tfoley.join_the_illagers.datagen.*;
 
 public class JoinTheIllagersDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
